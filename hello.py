90c50e80a44836b081bceb1937eb5a6a5ad9354e
叶浩 <yehao@withfield.tech>
2019-07-19 17:06:47 +0800

    string service demo

--- hello.py
+++ hello.py
@@ -20,3 +20,13 @@ if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
+
+
+"""
+步骤解析：
+    1.使用tornado.options模块解析命令行
+    2.创建tornado的Allication类的实例
+    3.handlers参数可以按需要指定任意多个元组()
+        - 元组中包括HTTP请求的路径和一个RequestHandler类
+        - 字符串"/"被看作为"^/$"
+"""
